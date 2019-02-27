71c72fc8a222117a3daea3678d9e14f3f2fc5b12
Lorena Costa <lorenacostace@gmail.com>
2019-02-27 20:12:37 +0100

    State. 27

--- src/App.js
+++ src/App.js
@@ -5,52 +5,23 @@ export default class CharacterCard extends Component{
 
   constructor(props){
     super(props);
-    console.log('constructor');
-
-    // Creamos un estado
-    this.state = {app: true}
+    this.state = {
+      state: this.props.state,
+      chapters: this.props.chapters
+    }
   }
 
-  // Se ejecuta cuando se ha terminado de montar
   componentDidMount(){
-    this.setState({app: false}); // Cuando acabe de montarse, cambiamos el estado a false
-    console.log('did mount');
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot){
-    console.log('did update');
-  }
-
-  componentWillUnmount(){
-    console.log('will unmount');
-  }
-
-  // Comprueba si el estado ha sido modificado.
-  // Si es true se ejecuta el render, y si es false no se ejecuta el render. Si no ejecuta el render, entonces no se
-  //comprueba el estado, y se ejecuta App
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('should??');
-    return false;
-  }
-
-  // Se necesita crear un estado inicial (ahora no tiene sentido), y debe devolver algo
-  static getDerivedStateFromProps(props, state){
-    console.log('getDerivedStateFromProps');
-    return null;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState){
-    console.log('getSnapshotBeforeUpdate');
+    this.setState({state:"dead"});
   }
 
   render() {
     return (
         <div className="card">
-          { this.state.app ? <App/> : <span></span> }
-          <h2>Nombre personaje</h2>
-          <p>Estado</p>
-          <p>Género</p>
-          <p>Capítulos</p>
+          <App titulo={this.props.titulo}/>
+          <p>{this.state.state}</p>
+          <p>{this.props.gender}</p>
+          <p>{this.state.chapters}</p>
 
         </div>
     );
@@ -58,19 +29,11 @@ export default class CharacterCard extends Component{
 }
 
 export class App extends Component {
-  componentDidMount(){
-    this.setState({app: false}); // Cuando acabe de montarse, cambiamos el estado a false
-    console.log('App: did mount');
-  }
-
-  componentWillUnmount(){
-    console.log('App: will unmount');
-  }
 
   render() {
     return (
       <div className="rym">
-        <h1>Rick y Morty</h1>
+        <h1>{this.props.titulo}</h1>
 
       </div>
     );
