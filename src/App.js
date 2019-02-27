a7dcc94cfa6c9d9d5171fb6cc39039e6da6f7d9d
Lorena Costa <lorenacostace@gmail.com>
2019-02-27 18:50:18 +0100

    Ciclo de vida Componentes (2). 25

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class CharacterCard extends Component{
+export default class CharacterCard extends Component{
+
+  constructor(props){
+    super(props);
+    console.log('constructor');
+
+    // Creamos un estado
+    this.state = {app: true}
+  }
+
+  // Se ejecuta cuando se ha terminado de montar
+  componentDidMount(){
+    this.setState({app: false}); // Cuando acabe de montarse, cambiamos el estado a false
+    console.log('did mount');
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('did update');
+  }
+
+  componentWillUnmount(){
+    console.log('will unmount');
+  }
+
+  // Comprueba si el estado ha sido modificado.
+  // Si es true se ejecuta el render, y si es false no se ejecuta el render. Si no ejecuta el render, entonces no se
+  //comprueba el estado, y se ejecuta App
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('should??');
+    return false;
+  }
+
+  // Se necesita crear un estado inicial (ahora no tiene sentido), y debe devolver algo
+  static getDerivedStateFromProps(props, state){
+    console.log('getDerivedStateFromProps');
+    return null;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('getSnapshotBeforeUpdate');
+  }
+
   render() {
     return (
         <div className="card">
+          { this.state.app ? <App/> : <span></span> }
           <h2>Nombre personaje</h2>
           <p>Estado</p>
           <p>Género</p>
@@ -16,7 +57,16 @@ class CharacterCard extends Component{
   }
 }
 
-class App extends Component {
+export class App extends Component {
+  componentDidMount(){
+    this.setState({app: false}); // Cuando acabe de montarse, cambiamos el estado a false
+    console.log('App: did mount');
+  }
+
+  componentWillUnmount(){
+    console.log('App: will unmount');
+  }
+
   render() {
     return (
       <div className="rym">
@@ -26,5 +76,3 @@ class App extends Component {
     );
   }
 }
-
-export default CharacterCard;
