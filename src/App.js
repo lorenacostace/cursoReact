6a3164a1360daadb2fc1a9489eba37ec4decc126
Lorena Costa <lorenacostace@gmail.com>
2019-02-28 19:14:15 +0100

    Mejoras al proyecto(4). 33

--- src/App.js
+++ src/App.js
@@ -25,12 +25,28 @@ export default class Characters extends Component{
       this.setState({characters: [...this.state.characters, character]})
     };
 
+    rmCharacter = (name) => {
+        let copy = [...this.state.characters];
+        let index = -1;
+
+        copy.forEach((ch, i) => {
+           if (ch.name === name){
+               index = i
+           }
+        });
+
+        if(index !== -1){
+            copy.splice(index, 1); // Para borrar
+            this.setState({characters: copy})
+        }
+    };
+
     render() {
         return(
             <div>
                 <Form addCharacter = {this.addCharacter} name="rick" />
                 {this.state.characters.map((ch, i) =>
-                    <CharacterCard key={i} titulo={ch.name} state={ch.status} gender={ch.gender} chapters={this.extractChapters(ch.episode)} />
+                    <CharacterCard rmCharacter={this.rmCharacter} key={i} titulo={ch.name} state={ch.status} gender={ch.gender} chapters={this.extractChapters(ch.episode)} />
                 )}
             </div>
         );
@@ -53,13 +69,19 @@ export class CharacterCard extends Component{
 
   kill = (param, e) =>{
       console.log(e.target.value, param);
-      this.setState({state:e.target.value});
+      this.setState({state: e.target.value});
+  };
+
+  rmCharacter = (e) => {
+    console.log("Props", this.props.titulo);
+    this.props.rmCharacter(this.props.titulo) // Lo llamamos asi porque en el render del padre, titulo={ch.name}
   };
 
   render() {
       console.log(this.state.characters);
     return (
         <div className="card">
+          <button onClick={this.rmCharacter}>Eliminar</button>
           <App setApp={this.setApp} titulo={this.props.titulo}/>
           <p>{this.state.state}</p>
           <p>{this.props.gender}</p>
